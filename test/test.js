2e7dcd8e38766d77fe7a82b80709b7ae425645bf
WuQic <chao.memo@gmail.com>
2018-04-12 20:11:31 +0800

    fix test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const HiveJdbc = require('../src/index')
+const HiveJdbc = require('../index')
 
 const conf = {
   url: 'jdbc:hive2://192.168.0.223:10000/default',
@@ -18,8 +18,8 @@ const conf = {
 const main = async () => {
   const hive = new HiveJdbc(conf)
   // const conn = await hive.getConnection()
-  // const res = await hiveKit.runQuery('select * from users where userid=? limit 10', ['c81e728d9d4c2f636f067f89cc14862c'])
-  const res = await hive.runQuery('select count(*) from users')
+  // const res = await hive.runQuery('select * from users where userid=? limit 10', ['c81e728d9d4c2f636f067f89cc14862c'])
+  const res = await hive.runQuery('select * from users where userId="c81e728d9d4c2f636f067f89cc14862c"')
   console.log(res)
 }
 
